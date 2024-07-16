f36afb4e0f85996e93a22000a7ef8cee680da44e
Jim Curlee <jamescurlee@gmail.com>
2024-07-16 14:45:00 -0600

    feat: add migration to generate deposit table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_043029) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_204029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deposits", force: :cascade do |t|
+    t.datetime "date", null: false
+    t.decimal "amount", precision: 8, scale: 2
+    t.bigint "tradeline_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tradeline_id"], name: "index_deposits_on_tradeline_id"
+  end
 
   create_table "tradelines", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_043029) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "deposits", "tradelines"
 end
