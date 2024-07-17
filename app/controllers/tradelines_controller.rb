6e594bf9970fb0b809889e616d4ee5f81f9cdd15
Jim Curlee <jamescurlee@gmail.com>
2024-07-17 08:29:19 -0600

    test: add tradeline create method to enable postman testing

--- app/controllers/tradelines_controller.rb
+++ app/controllers/tradelines_controller.rb
@@ -9,9 +9,23 @@ class TradelinesController < ApplicationController
     render json: Tradeline.find(params[:id])
   end
 
+  def create
+    @tradeline = Tradeline.new(tradeline_params)
+
+    if @tradeline.save
+      render json: @tradeline, status: :created
+    else
+      render json: @tradeline.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def not_found
     render json: 'not_found', status: :not_found
+  end
+
+  def tradeline_params
+    params.require(:tradeline).permit(:name, :amount)
   end
 end
